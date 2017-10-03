2f395d9038617c95a9363b3001ae278fc457b899
jiafeng <283678339@qq.com>
2017-10-03 10:37:27 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,16 @@
 <html>
 <head>
     <title>@yield('title', 'Jyf') - 假疯正式开始的第一个应用</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
     @yield('content')
+    @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+<h1>欢迎来到JYF的应用</h1>
+<p class="lead">
+    你现在看到的是<a href="tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=283678339&website=www.oicqzone.com">假疯</a>学PHP的入门网页的示例项目主页。
+</p>
+<p>
+    永远相信，美好的事情即将发生。
+</p>
+<p>
+    <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+</p>
+</div>
 @stop
