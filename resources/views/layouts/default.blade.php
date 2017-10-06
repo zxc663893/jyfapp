2c8795707c42f8f809215448c40cdda546d7ea91
jiafeng <283678339@qq.com>
2017-10-06 11:17:44 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,8 +9,9 @@
 
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
-    @yield('content')
-    @include('layouts._footer')
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
         </div>
     </div>
 </body>
